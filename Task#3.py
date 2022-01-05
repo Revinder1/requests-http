008ee4764dae9bbac79e75c017e88774bcbdcbee
Yaroslav <67860564+Revinder1@users.noreply.github.com>
2022-01-06 02:36:22 +0500

    Update Task#3.py

--- Task#3.py
+++ Task#3.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from pprint import pprint
 
 
-def get_questions(days, tag):
+def get_questions(days):
     current_date = int(datetime.timestamp(datetime.now()))  # Время в секундах, начиная с 1970 года по текущую секунду
     start_date = current_date - days * 86400  # В сутках 86400 секунд
     params = {
         'fromdate': start_date,
         'todate': current_date,
-        'tagged': tag,
+        'tagged': 'Python',
         'site': 'stackoverflow'
     }
     resp = requests.get('https://api.stackexchange.com/2.3/questions', params=params)
@@ -20,4 +20,4 @@ def get_questions(days, tag):
     # pprint(resp.json())
 
 
-get_questions(2, 'python')
+get_questions(2)
